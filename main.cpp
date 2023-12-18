a7d37deedbed17e7f6bd463ef49b14ebfbb21647
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2023-12-18 21:00:45 +0700

    par2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <string>
+#include <bits/stdc++.h>
 #include "mpi/mpi.h"
 
 class TravellingSalesman {
@@ -33,31 +35,38 @@ public:
     }
 };
 
-int main() {
+int main(int argc, char *argv[]) {
     MPI_Init(nullptr, nullptr);
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(1, 40);
 
-    int n = 13;
+    int n = std::stoi(argv[1]);
 
-    std::vector<std::vector<int>> distanceMatrix(n, std::vector<int>(n)); /*= {
-            {0, 16, 14, 26, 27},
-            {16, 0, 17, 30, 26},
-            {14, 17, 0, 1, 12},
-            {26, 30, 1, 0, 26},
-            {27, 26, 12, 26, 0},
-    };*/
+    std::vector<std::vector<int>> distanceMatrix/*(n, std::vector<int>(n));*/ = {
+            {0, 37, 32, 18, 22, 4, 3, 15, 23, 28, 27, 23},
+            {37, 0, 5, 19, 13, 22, 13, 39, 36, 22, 17, 29},
+            {32, 5, 0, 24, 40, 31, 26, 12, 30, 28, 31, 19},
+            {18, 19, 24, 0, 19, 3, 4, 21, 36, 33, 15, 14},
+            {22, 13, 40, 19, 0, 31, 37, 5, 23, 21, 29, 36},
+            {4, 22, 31, 3, 31, 0, 4, 35, 21, 32, 34, 16},
+            {3, 13, 26, 4, 37, 4, 0, 28, 31, 16, 40, 7},
+            {15, 39, 12, 21, 5, 35, 28, 0, 37, 34, 9, 22},
+            {23, 36, 30, 36, 23, 21, 31, 37, 0, 38, 34, 38},
+            {28, 22, 28, 33, 21, 32, 16, 34, 38, 0, 24, 39},
+            {27, 17, 31, 15, 29, 34, 40, 9, 34, 24, 0, 24},
+            {23, 29, 19, 14, 36, 16, 7, 22, 38, 39, 24, 0}
+    };
 
-    for (int i = 0; i < n; i++) {
-        distanceMatrix[i][i] = 0; // set diagonal to 0
-        for (int j = i + 1; j < n; j++) {
-            int randomValue = dis(gen);
-            distanceMatrix[i][j] = randomValue;
-            distanceMatrix[j][i] = randomValue; // set symmetric value
-        }
-    }
+//    for (int i = 0; i < n; i++) {
+//        distanceMatrix[i][i] = 0; // set diagonal to 0
+//        for (int j = i + 1; j < n; j++) {
+//            int randomValue = dis(gen);
+//            distanceMatrix[i][j] = randomValue;
+//            distanceMatrix[j][i] = randomValue; // set symmetric value
+//        }
+//    }
 
     // Print generated matrix
     for (const auto &row : distanceMatrix) {
---
+++ par2.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+#include "mpi/mpi.h"
+
+using namespace std;
+
+vector<int> fact;
+
+vector<int> nth_permutation(vector<int> &arr, long long n) {
+    int N = arr.size();
+
+    sort(arr.begin(), arr.end());
+
+    set<int> st;
+    for (int x: arr) st.insert(x);
+
+    vector<int> ans;
+
+    for (int i = 0; i < N; i++) {
+        int cn = 1;
+        int cval = fact[N - 1 - i];
+
+        while (cval < n) {
+            cn++;
+            cval *= cn;
+            cval /= (cn - 1);
+        }
+
+        int pval = cval * (cn - 1) / cn;
+        n -= pval;
+
+        auto it = st.begin();
+        for (int i = 0; i < cn - 1; i++)it++;
+        ans.push_back(*it);
+        st.erase(it);
+    }
+
+    return ans;
+}
+
+int findCost(int **matrix, vector<int> &arr) {
+    int cost = 0;
+
+    for (int i = 1; i < (int) arr.size(); i++)
+        cost += matrix[arr[i]][arr[i - 1]];
+
+    return cost;
+}
+
+int main(int argc, char *argv[]) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(1, 40);
+    int n = stoi(argv[1]);
+
+    int size;
+    int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    fact.resize(n);
+    fact[0] = 1;
+    for (int i = 1; i < n; i++)
+        fact[i] = i * fact[i - 1];
+
+    int **matrix = new int *[n];
+    matrix[0] = new int[n * n];
+    for (int i = 1; i < n; i++)
+        matrix[i] = matrix[i - 1] + n;
+
+    if (rank == 0) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][i] = 0;
+            for (int j = i + 1; j < n; j++) {
+                int randomValue = dis(gen);
+                matrix[i][j] = randomValue;
+                matrix[j][i] = randomValue;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            std::cout << "{";
+            for (int i = 0; i < n; i++) {
+                std::cout << matrix[j][i];
+                if (i != n - 1)
+                    std::cout << ", ";
+                else
+                    std::cout << "}";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    MPI_Bcast(*matrix, n * n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    double start = MPI_Wtime();
+
+    int optimal_value = INT32_MAX;
+
+    // divide among PEs
+
+    int permPerProc = fact[n - 1] / size;
+    int rem = fact[n - 1] % size;
+
+    int startPermInd, endPermInd;
+
+    vector<int> route(n - 1);
+    for (int i = 1; i < n; i++)
+        route[i - 1] = i;
+
+    if (rem == 0) {
+        startPermInd = (rank * permPerProc) + 1;
+        endPermInd = (rank + 1) * permPerProc;
+    } else {
+        if (rank < rem) {
+            startPermInd = (rank * (permPerProc + 1)) + 1;
+            endPermInd = (rank + 1) * (permPerProc + 1);
+        } else {
+            startPermInd = rem * (permPerProc + 1) + (rank - rem) * permPerProc + 1;
+            endPermInd = rem * (permPerProc + 1) + (rank + 1 - rem) * permPerProc;
+        }
+    }
+
+    vector<int> minRoute(n + 1);
+
+    vector<int> routeBegin = nth_permutation(route, startPermInd);
+    vector<int> routeEnd = nth_permutation(route, endPermInd);
+
+    do {
+        vector<int> temp = routeBegin;
+        temp.push_back(0);
+        temp.insert(temp.begin(), 0);
+        int val = findCost(matrix, temp);
+
+        if (val < optimal_value) {
+            optimal_value = val;
+            minRoute = temp;
+        }
+
+        if (routeBegin == routeEnd)
+            break;
+
+    } while (next_permutation(routeBegin.begin(), routeBegin.end()));
+
+    MPI_Gather(&minRoute[0], n, MPI_INT, matrix[0], n, MPI_INT, 0, MPI_COMM_WORLD);
+    vector<int> answers(size);
+    MPI_Gather(&optimal_value, 1, MPI_INT, &answers[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        int minIndex = 0;
+        for (int i = 1; i < size; i++) {
+            if (answers[i] < answers[minIndex])
+                minIndex = i;
+        }
+
+        double final = MPI_Wtime();
+
+        cout << "\nMin route = ";
+        for (int i = 0; i < n; i++)
+            cout << matrix[minIndex][i] << ' ';
+        cout << "; min way length = " << answers[minIndex] << endl;
+        cout << "\nTime spent = " << final - start << endl;
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
